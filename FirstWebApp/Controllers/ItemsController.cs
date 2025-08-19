79e357e0ab0474f292b5e8fc1113d358f00417d8
Elkholy-m <midoelkholy03@gmail.com>
2025-08-19 11:06:14 +0300

    ADD Partial Server Client Validation With shared/_Validation

--- FirstWebApp/Controllers/ItemsController.cs
+++ FirstWebApp/Controllers/ItemsController.cs
@@ -18,6 +18,29 @@ namespace FirstWebApp.Controllers
             IEnumerable<Item> _items = _dbContext.items.ToList();
             return View(_items);
         }
+        [HttpGet]
+        public IActionResult New()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult New(Item _item)
+        {
+            if (_item.Price.Equals(decimal.TryParse(_item.Name, out decimal x) ? x : decimal.MaxValue))
+            {
+                ModelState.AddModelError("Name", "Price Can't Be Equals Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _dbContext.items.Add(_item);
+                _dbContext.SaveChanges();
+                 return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(_item);
+            }
+        }
         public IActionResult Error()
         {
             return View(new ErrorViewModel {  RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
