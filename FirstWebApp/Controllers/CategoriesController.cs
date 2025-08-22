737c19b7245e48f45d7c4522683964e5337740df
Elkholy-m <midoelkholy03@gmail.com>
2025-08-22 09:12:36 +0300

    Add Categories Controller Using Repository Pattern

---
+++ FirstWebApp/Controllers/CategoriesController.cs
@@ -0,0 +1,20 @@
+﻿using FirstWebApp.Models;
+using FirstWebApp.MyRepository.Base;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApp.Controllers
+{
+    public class CategoriesController :Controller
+    {
+        private readonly IRepository<Category> _repository;
+
+        public CategoriesController(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+        public IActionResult Index()
+        {
+            return View(_repository.FindAll());
+        }
+    }
+}
--- FirstWebApp/MyRepository/Repository.cs
+++ FirstWebApp/MyRepository/Repository.cs
@@ -11,6 +11,12 @@ namespace FirstWebApp.MyRepository
         {
             this.context = context;
         }
+
+        public IEnumerable<T> FindAll()
+        {
+            return context.Set<T>().ToList();
+        }
+
         public T FindById(int id)
         {
             return context.Set<T>().Find(id) ?? throw new Exception("Entity Not Found");
